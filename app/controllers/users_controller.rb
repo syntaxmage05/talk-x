10858f48a175a985299e2c72007efb63f0f68830
devmuturi <muturinjonge09@gmail.com>
2026-01-11 16:01:02 +0300

    Fix the lint

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show]
+  before_action :set_user, only: [ :show ]
   def show
   end
 
@@ -13,14 +13,14 @@ class UsersController < ApplicationController
       flash[:success] = "Welcome to the Talk X!"
       redirect_to @user
     else
-      render 'new', status: :unprocessable_entity
+      render "new", status: :unprocessable_entity
     end
   end
 
   private
 
   def user_params
-    params.expect(user: [:name, :email, :password, :password_confirmation])
+    params.expect(user: [ :name, :email, :password, :password_confirmation ])
   end
 
   def set_user
